2809d2f96ef7c0843db37cbbf2a04cecea8b46df
todelusion <todelusion@gmail.com>
2022-11-11 13:04:13 +0800

    init

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,8 +4,7 @@ import Nav from "./Layout/Nav";
 export default function Router(): JSX.Element {
   return (
     <Routes>
-      <Route path="/" element={<Nav />}>
-      </Route>
+      <Route path="/" element={<Nav />} />
     </Routes>
   );
 }
