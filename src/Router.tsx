2abee4e268ddbf2e5f0e6db426b39ff3578e823e
s10614165 <kweifon1998@gmail.com>
2022-11-12 16:13:33 +0800

    feat Ladingpage

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Nav from "./Layout/Nav";
 import Home from "./pages/Home";
+import LandingPage from "./pages/LandingPage ";
 
 export default function Router(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Nav />}>
+      <Route path="/" element={<LandingPage />} />
         <Route path="/" element={<Home />} />
       </Route>
     </Routes>
--- src/pages/LandingPage .tsx
+++ src/pages/LandingPage .tsx
@@ -1,11 +1,24 @@
 import { Outlet } from "react-router-dom";
-
+import PesImg from '../img/Pets.svg'
 function LandingPage(): JSX.Element {
 
   return (
-    <>
-     
-    </>
+    <div className="display:flex flex-direction:column">
+     <header>
+    1
+     </header>
+  <main className="display:flex flex-direction:column">
+    <section>1</section>
+    <section>2</section>
+    <section>3</section>
+  </main>
+  <div className="display:flex flex-direction:column">
+  <p className="text-5xl">寵物坊城市，只為您與毛小孩著想</p>
+    <button>了解更多</button>
+    <button><img src="{PesImg}" alt="" /> 立即註冊試用</button>
+  </div>
+   
+  </div>
   );
 }
 export default LandingPage;
