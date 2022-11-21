afa6c4e37d091a327e90ee348fb83b037998b9de
todelusion <todelusion.gmail.com>
2022-11-22 01:02:41 +0800

    test SearchBar component in Contex

--- src/pages/ContextTest.tsx
+++ src/pages/ContextTest.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import DatePicker from "../components/DatePicker";
-import useComponent from "../hooks/useComponent";
+import SearchBar from "../components/SearchBar";
 
 function ContextTest(): JSX.Element {
   return (
     <div className="flex-col-center pt-40">
       <Link to="/home">點此返回home頁</Link>
-      <DatePicker />
+      <SearchBar />
     </div>
   );
 }
