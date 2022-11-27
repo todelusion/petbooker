b0b8a331b0cf1984cd9f96558078fdb4166d0cc9
todelusion <todelusion@gmail.com>
2022-11-27 21:27:00 +0800

    add Info component in /pages/Hotel

--- src/pages/Hotel/index.tsx
+++ src/pages/Hotel/index.tsx
@@ -2,6 +2,21 @@ import { useParams } from "react-router-dom";
 import { Hotels } from "../../components/HotelCard/data";
 import SearchBar from "../../containers/SearchBar/SearchBar";
 import Image from "./Image";
+import Info from "./Info";
+
+export interface IHotel {
+  Id: string;
+  HotelName: string;
+  HotelPhone: string;
+  HotelAddress: string;
+  HotelStartTime: string;
+  HotelEndTime: string;
+  HotelInfo: string;
+  HotelPhoto: string[];
+  AreaId: string;
+  price: number;
+  HotelScore: number;
+}
 
 function Hotel(): JSX.Element {
   const { id } = useParams();
@@ -12,6 +27,7 @@ function Hotel(): JSX.Element {
     <div className="flex-col-center px-20 pt-40">
       <SearchBar className="mb-12" />
       <Image hotel={hotel} className="mb-12" />
+      <Info hotel={hotel} />
     </div>
   );
 }
