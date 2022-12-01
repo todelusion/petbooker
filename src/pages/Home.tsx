39d4cba9f56a1c6e0114112de37acff8cb33f882
todelusion <todelusion@gmail.com>
2022-12-01 18:31:14 +0800

    fixed Modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,39 +9,36 @@ import StatusModal from "../Layout/StatusModal";
 
 let render = 0;
 
-const Home = React.memo((): JSX.Element => {
+function Home(): JSX.Element {
   const { pending, dispatchPending } = usePending();
   render += 1;
   console.log(render);
 
   return (
-    <>
-      <StatusModal pending={pending} />
-      <div className="relative flex w-full items-start justify-evenly px-20 pt-40">
-        <button
-          type="button"
-          onClick={() => {
-            if (pending.status === "") {
-              dispatchPending({ type: "IS_SUCCESS", payload: "成功" });
-              return undefined;
-            }
+    <div className="relative flex w-full items-start justify-evenly px-20 pt-40">
+      <button
+        type="button"
+        onClick={() => {
+          if (pending.status === "") {
+            dispatchPending({ type: "IS_SUCCESS", payload: "成功" });
+            return undefined;
+          }
 
-            return dispatchPending({ type: "CLOSE_ALL" });
-          }}
-        >
-          Toggle
-        </button>
-        <Filter className="max-w-xs" />
-        <div className="flex max-w-2xl flex-col items-center xl:max-w-4xl">
-          <SearchBar className="mb-16" />
-          <div className="flex flex-col items-end">
-            <DropDownList className="mb-3" />
-            <HotelCard />
-          </div>
+          return dispatchPending({ type: "CLOSE_ALL" });
+        }}
+      >
+        Toggle
+      </button>
+      <Filter className="max-w-xs" />
+      <div className="flex max-w-2xl flex-col items-center xl:max-w-4xl">
+        <SearchBar className="mb-16" />
+        <div className="flex flex-col items-end">
+          <DropDownList className="mb-3" />
+          <HotelCard />
         </div>
       </div>
-    </>
+    </div>
   );
-});
+}
 
 export default Home;
