a68804dafa8fdc87528a412c4752162d7a2c60bf
todelusion <todelusion@gmail.com>
2022-12-03 22:44:24 +0800

    add Order component

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,7 +1,8 @@
 const buttonStyle = {
   Primary: "border-2 border-primary text-black",
   Secondary: "bg-second text-white border-2 border-second",
-  Transparent: "border-2 hover:border-black text-black duration-75",
+  Transparent:
+    "border-2 hover:border-black hover:font-bold hover:bg-stone-200 duration-100",
 };
 
 export default buttonStyle;
--- src/pages/Cms/CmsOrder/index.tsx
+++ src/pages/Cms/CmsOrder/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Button from "../../../components/Button";
+import Order from "../../../components/Order";
+import { cmsOrder } from "../../../components/Order/data";
 
 function CmsOrder(): JSX.Element {
   return (
-    <div className=" w-full">
+    <>
       <nav className="flex">
         <Button type="Transparent" text="待入住" className="py-3 px-6" />
         <Button type="Transparent" text="已入住" className="ml-4 py-3 px-6" />
         <Button type="Transparent" text="完成訂單" className="ml-4 py-3 px-6" />
         <Button type="Transparent" text="取消訂單" className="ml-4 py-3 px-6" />
       </nav>
-      <h2>CmsOrder</h2>
-    </div>
+      <Order data={cmsOrder} />
+    </>
   );
 }
 
